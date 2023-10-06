3f4a9c318cdb129b880f221e8a7d196550e8290f
Denny09310 <k.denny2000@hotmail.com>
2023-10-06 21:38:08 +0200

    Added prettier and formatted code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,13 @@
-import { createRoot } from "react-dom/client";
-import { Provider as ReduxStoreProvider } from "react-redux";
+import { createRoot } from 'react-dom/client';
+import { Provider as ReduxStoreProvider } from 'react-redux';
 
-import App from "~/app";
-import { store } from "~/app/store";
+import App from '~/app';
+import { store } from '~/app/store';
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <ReduxStoreProvider store={store}>
     <App />
-  </ReduxStoreProvider>
+  </ReduxStoreProvider>,
 );
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,14 +1,8 @@
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-} from "@ionic/react";
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 
-import ExploreContainer from "~/components/ExploreContainer";
+import ExploreContainer from '~/components/ExploreContainer';
 
-import "./Home.css";
+import './Home.css';
 
 const Home: React.FC = () => {
   return (
