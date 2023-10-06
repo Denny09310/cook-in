aa0be28a161f2ccf633b99cf4d94c69187889fa0
Denny09310 <k.denny2000@hotmail.com>
2023-10-06 22:08:20 +0200

    Added first endpoint

---
+++ api/Endpoints/HelloWorld/Endpoint.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Endpoints.HelloWorld;
+
+[HttpGet("/hello-world"), AllowAnonymous]
+sealed class Endpoint : EndpointWithoutRequest<Response>
+{
+    public override Task HandleAsync(CancellationToken ct)
+    {
+        return SendOkAsync(Response, ct);
+    }
+}
