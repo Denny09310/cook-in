54fd50abe31f4e04bdd85e2fa45b8cf8e495abd8
Denny09310 <k.denny2000@hotmail.com>
2023-10-07 09:19:46 +0200

    Added support for MySql and EF Core

---
+++ api/Extensions/DbContextExtensions.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+using Recipe.Io.Data;
+
+namespace Recipe.Io.Extensions;
+
+internal static class DbContextExtensions
+{
+    internal static IServiceCollection ConfigureDbContext(this IServiceCollection services, string connectionName) =>
+        services.AddDbContextPool<ApplicationDbContext>((sp, opt) =>
+        {
+            var configuration = sp.GetRequiredService<IConfiguration>();
+            var connectionString = configuration.GetConnectionString("Default");
+
+            ArgumentException.ThrowIfNullOrEmpty(connectionString);
+
+            var connectionStringBuilder = new MySqlConnectionStringBuilder(connectionString);
+            var dbPassword = configuration.GetValue<string>("DefaultDbPassword");
+            ArgumentException.ThrowIfNullOrEmpty(dbPassword);
+
+            connectionStringBuilder.Password = dbPassword;
+            connectionString = connectionStringBuilder.ToString();
+
+            opt.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        });
+}
--- api/Program.cs
+++ api/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddFastEndpoints();
 builder.Services.ConfigureSwaggerDocument();
+builder.Services.ConfigureDbContext("Default");
 
 builder.Services.AddGraphQLServer()
     .AddQueryType<Query>();
