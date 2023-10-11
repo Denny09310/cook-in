85cfe6e1563c07dd1f8f992e40964d6dcb166f99
Denny09310 <k.denny2000@hotmail.com>
2023-10-11 18:28:40 +0200

    added persistent storage

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import { Provider as ReduxStoreProvider } from 'react-redux';
 import { register } from 'swiper/element/bundle';
 
 import App from '~/App';
-import { store } from '~/app/store';
+import store from '~/app/store';
 import AuthProvider from '~/components/providers/AuthProvider';
 import ThemeProvider from '~/components/providers/ThemeProvider';
 
