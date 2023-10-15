2a1a68f4faf1f979a589ed38adbc2368b07ed3a0
Denny09310 <k.denny2000@hotmail.com>
2023-10-15 11:53:23 +0200

    styled header and added buttons

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,10 @@
 import {
   InfiniteScrollCustomEvent,
+  IonButton,
+  IonButtons,
   IonContent,
   IonHeader,
+  IonIcon,
   IonInfiniteScroll,
   IonInfiniteScrollContent,
   IonLoading,
@@ -11,13 +14,15 @@ import {
   IonTitle,
   IonToolbar,
   RefresherCustomEvent,
-  RefresherEventDetail,
 } from '@ionic/react';
+import clsx from 'clsx';
 import React from 'react';
 
-import RecipesInfiniteList from '~/components/RecipesInfiniteList';
-import { useGetRecipesQuery } from '~/app/services/recipes';
+import { menu, notifications } from 'ionicons/icons';
 import { useCounter } from 'react-use';
+import { useGetRecipesQuery } from '~/app/services/recipes';
+import RecipesInfiniteList from '~/components/RecipesInfiniteList';
+import styles from './Home.module.scss';
 
 const Home: React.FC = () => {
   const [page, { inc: nextPage }] = useCounter(1);
@@ -34,7 +39,19 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader className="ion-no-border">
         <IonToolbar>
-          <IonTitle>Page Title</IonTitle>
+          <IonButtons slot="start">
+            <IonButton>
+              <IonIcon slot="icon-only" icon={menu} />
+            </IonButton>
+          </IonButtons>
+          <IonTitle class={clsx('ion-text-center', styles.title)}>
+            Cook<span className={styles['highlight-primary']}>In</span>
+          </IonTitle>
+          <IonButtons slot="end">
+            <IonButton>
+              <IonIcon slot="icon-only" icon={notifications} />
+            </IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent>
