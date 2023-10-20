593826468de05e25c44df6924a6d14f57bdf64f7
Denny09310 <k.denny2000@hotmail.com>
2023-10-20 21:02:42 +0200

    updated sw

--- client/src/service-worker.ts
+++ client/src/service-worker.ts
@@ -1,25 +1,34 @@
+// Service worker
+//
+// References
+// https://github.com/webmaxru/pwatter/blob/workbox/src/sw-default.js
+// Caching strategies: https://developers.google.com/web/tools/workbox/modules/workbox-strategies#stale-while-revalidate
+// Example: https://github.com/JeremieLitzler/mws.nd.2018.s3/blob/master/sw.js
+
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
 import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching';
-import { BackgroundSyncPlugin } from 'workbox-background-sync';
 import { registerRoute } from 'workbox-routing';
-import { NetworkOnly } from 'workbox-strategies';
+import { CacheFirst, NetworkOnly, StaleWhileRevalidate } from 'workbox-strategies';
 
-import { version } from '../package.json';
-
-declare let self: ServiceWorkerGlobalScope;
-
-const componentName = 'Service Worker';
+declare const self: any;
 
 // Enable debug mode during development
 const DEBUG_MODE = location.hostname.endsWith('.app.local') || location.hostname === 'localhost';
-const DAYS_UNIT = 24 * 60;
+
+const DAY_IN_SECONDS = 24 * 60 * 60;
+const MONTH_IN_SECONDS = DAY_IN_SECONDS * 30;
+const YEAR_IN_SECONDS = DAY_IN_SECONDS * 365;
 
 /**
  * The current version of the service worker.
  */
-const SERVICE_WORKER_VERSION = DEBUG_MODE ? '0.0.0' : version;
+const SERVICE_WORKER_VERSION = '1.0.0';
 
 if (DEBUG_MODE) {
-  console.debug(`Service worker version ${SERVICE_WORKER_VERSION} loading...`);
+  console.debug(
+    `🚀 ~ service-worker ~ Service worker version ${SERVICE_WORKER_VERSION} loading...`,
+  );
 }
 
 // ------------------------------------------------------------------------------------------
@@ -36,22 +45,66 @@ const assetsToCache = self.__WB_MANIFEST;
 //assetsToCache = [...assetsToCache, ???];
 
 if (DEBUG_MODE) {
-  console.trace(`${componentName}:: Assets that will be cached: `, assetsToCache);
+  console.trace('🚀 ~ service-worker.ts ~ Assets that will be cached:', assetsToCache);
 }
 
 precacheAndRoute(assetsToCache);
 
+// ------------------------------------------------------------------------------------------
+// Routes
+// ------------------------------------------------------------------------------------------
+
+// Cache the Google Fonts stylesheets with a stale while revalidate strategy.
 registerRoute(
-  /\/api\/.*\/*.json/,
-  new NetworkOnly({
+  /^https:\/\/fonts\.googleapis\.com/,
+  new StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  }),
+);
+
+// Cache the Google Fonts webfont files with a cache first strategy for 1 year.
+registerRoute(
+  /^https:\/\/fonts\.gstatic\.com/,
+  new CacheFirst({
+    cacheName: 'google-fonts-webfonts',
     plugins: [
-      new BackgroundSyncPlugin('apiRequestsQueue', {
-        maxRetentionTime: DAYS_UNIT,
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: YEAR_IN_SECONDS,
+        maxEntries: 30,
+        purgeOnQuotaError: true, // Automatically cleanup if quota is exceeded.
       }),
     ],
   }),
-  'POST',
 );
+
+// Make JS/CSS fast by returning assets from the cache
+// But make sure they're updating in the background for next use
+registerRoute(/\.(?:js|css)$/, new StaleWhileRevalidate());
+
+// Cache images
+// But clean up after a while
+registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  new CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 250,
+        maxAgeSeconds: MONTH_IN_SECONDS,
+        purgeOnQuotaError: true, // Automatically cleanup if quota is exceeded.
+      }),
+    ],
+  }),
+);
+
+// Anything authentication related MUST be performed online
+registerRoute(/(https:\/\/)?([^\/\s]+\/)api\/v1\/auth\/.*/, new NetworkOnly());
+
+// Database access is only supported while online
+registerRoute(/(https:\/\/)?([^\/\s]+\/)database\/.*/, new NetworkOnly());
 
 // ------------------------------------------------------------------------------------------
 // Messages
@@ -63,7 +116,8 @@ self.addEventListener('message', (event: { data: any; type: any; ports: any }) =
     if ('GET_VERSION' === event.data.type) {
       if (DEBUG_MODE) {
         console.debug(
-          `${componentName}:: Returning the service worker version: ${SERVICE_WORKER_VERSION}`,
+          '🚀 ~ service-worker.ts ~ Returning the service worker version:',
+          SERVICE_WORKER_VERSION,
         );
       }
       event.ports[0].postMessage(SERVICE_WORKER_VERSION);
@@ -74,7 +128,7 @@ self.addEventListener('message', (event: { data: any; type: any; ports: any }) =
     // Reference about why we wait: https://stackoverflow.com/questions/51715127/what-are-the-downsides-to-using-skipwaiting-and-clientsclaim-with-workbox
     if ('SKIP_WAITING' === event.data.type) {
       if (DEBUG_MODE) {
-        console.debug(`${componentName}:: Skipping waiting...`);
+        console.debug(`🚀 ~ service-worker.ts ~ Skipping waiting...`);
       }
       self.skipWaiting();
     }
@@ -83,7 +137,7 @@ self.addEventListener('message', (event: { data: any; type: any; ports: any }) =
     // of the service worker
     if ('CLIENTS_CLAIM' === event.data.type) {
       if (DEBUG_MODE) {
-        console.debug(`${componentName}:: Claiming clients and cleaning old caches`);
+        console.debug(`🚀 ~ service-worker.ts ~ Claiming clients and cleaning old caches`);
       }
       self.clients.claim();
     }
